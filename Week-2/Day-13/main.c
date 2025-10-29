873d4aef076ed4e9cee82dd5fcd835ac60f14da0
Obed Adu-Gyamfi <adugyamfiobed.tpp3@gmail.com>
2025-10-29 01:41:59 +0000

    Addition of coordinate shifts

--- Week-2/Day-13/main.c
+++ Week-2/Day-13/main.c
@@ -17,9 +17,13 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-    int dims[2] = {3, 2};
-    int periods[2] = {1, 1};
-    int reorder = false;
+    int dims[2] = {3, 2}; /* an array containing the structure of our 2-D grid, thus creating a 3 processes along  the first dimension (x) row
+                            and 2 processes along the sencond dimension (y) columnd */
+
+    int periods[2] = {1, 1}; /* a period of {1, 1} indicates a wrap around on  both axis, thus process loop around the edges to the neighbouring
+                             processes*/
+
+    int reorder = true;
 
     MPI_Dims_create(nprocs, 2, dims);
     MPI_Comm new_communicator;
@@ -29,8 +33,15 @@ int main(int argc, char **argv)
     int my_coords[2];
 
     MPI_Cart_coords(new_communicator, rank, 2, my_coords);
+    int up, down;
+
+    MPI_Cart_shift(new_communicator, 0, 1, &up, &down);
+
+    int left, right;
+    MPI_Cart_shift(new_communicator, 1, 1, &left, &right);
 
     printf("[MPI Process %d] I am located at (%d, %d).\n", rank, my_coords[0], my_coords[1]);
+    printf("[MPI Process %d] Up and down neighbours are (%d, %d) and lefft and right neigbours are (%d, %d)\n", rank, up, down, left, right);
     MPI_Finalize();
     return (0);
 }
